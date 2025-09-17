bc1980e78f9cc4bf38e0bcc84175a9e841c637a2
Anshaal-Kumar <anshaalkumar@gmail.com>
2025-09-17 20:11:29 +0000

    preload.js fix V3

--- app/preload.js
+++ app/preload.js
@@ -1,21 +1,54 @@
-// app/preload.js - Security Bridge
+// app/preload.js - Fixed Security Bridge
 const { contextBridge, ipcRenderer } = require('electron');
 
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
     // Server communication
-    getServerPort: () => ipcRenderer.invoke('get-server-port'),
+    getServerPort: () => {
+        console.log('Preload: Getting server port...');
+        return ipcRenderer.invoke('get-server-port');
+    },
     
     // File dialogs
-    showOpenDialog: (options) => ipcRenderer.invoke('show-open-dialog', options),
+    showOpenDialog: (options) => {
+        console.log('Preload: Opening file dialog...');
+        return ipcRenderer.invoke('show-open-dialog', options);
+    },
     
-    // Menu events
-    onFileSelected: (callback) => ipcRenderer.on('file-selected', callback),
+    // Menu events - using proper event handling
+    onFileSelected: (callback) => {
+        console.log('Preload: Setting up file selection listener...');
+        const wrappedCallback = (event, ...args) => callback(...args);
+        ipcRenderer.on('file-selected', wrappedCallback);
+        
+        // Return cleanup function
+        return () => {
+            ipcRenderer.removeListener('file-selected', wrappedCallback);
+        };
+    },
     
     // Cleanup
-    removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
+    removeAllListeners: (channel) => {
+        console.log(`Preload: Removing all listeners for ${channel}`);
+        ipcRenderer.removeAllListeners(channel);
+    },
+    
+    // Platform info
+    platform: process.platform,
+    
+    // App version info
+    versions: {
+        node: process.versions.node,
+        chrome: process.versions.chrome,
+        electron: process.versions.electron
+    }
 });
 
 contextBridge.exposeInMainWorld('appInfo', {
     version: '1.0.0',
     name: 'Digital Twin Intelligence Platform'
 });
+
+// Log when preload script loads
+console.log('Preload script loaded successfully');
